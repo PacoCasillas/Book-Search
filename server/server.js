91abbe64911c13dce93fb3bc262c28a4e65331e0
PacoCasillas <pacocasillasmx@gmail.com>
2023-07-26 15:07:53 -0400

    implement apollo server and appllied it to express

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// import auth middleware
+const { authMiddleware } = require('./utils/auth');
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+// import apollo server
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// integrate apollo server with express app as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +29,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
